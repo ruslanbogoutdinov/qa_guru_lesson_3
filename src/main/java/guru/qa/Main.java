d47ea7bddd2add22cf72fcc5cac9551c4fb07d0e
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-02 12:04:41 +0500

    Push calculate method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static int getNum(){
         return 5;
     }
+    public static int calculate(int num){
+        int result = 0;
+        for (int i = 0; i < num; i++){
+            result += i;
+        }
+        return result;
+    }
     public static void main(String[] args) {
         int counter = getNum();
         for (int i = 0; i < counter; i++) {
@@ -12,6 +19,7 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println("...");
         }
+        System.out.println("Entire amount is: "+calculate(getNum()));
         System.out.println("The end!");
     }
 }
