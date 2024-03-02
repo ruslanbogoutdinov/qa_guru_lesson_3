1e39dae5b816cc86ab52aeed4b449d31584ff48c
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-02 11:47:37 +0500

    Push third branch with some improves

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         int counter = getNum();
         for (int i = 0; i < counter; i++) {
-            System.out.println("Hello, world");
+            System.out.println("Hello, world!!!");
         }
+        System.out.println("The end!");
     }
 }
