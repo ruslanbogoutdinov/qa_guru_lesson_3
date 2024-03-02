bfabaa9de6126527d27682349d429c10e40ec17d
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-02 11:35:52 +0500

    init commit

---
+++ src/main/java/guru/qa/Main.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+public class Main {
+    public static int getNum(){
+        return 5;
+    }
+    public static void main(String[] args) {
+        int counter = getNum();
+        for (int i = 0; i < counter; i++) {
+            System.out.println("Hello, world");
+        }
+    }
+}
